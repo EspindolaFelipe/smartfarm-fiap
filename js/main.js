78b7cca9a09793a05fd9ec26f2ac63ceaee9c45e
Cristian <camesjaames@gmail.com>
2025-05-03 16:46:46 -0300

    Página Fale Conosco inserida.

--- js/main.js
+++ js/main.js
@@ -1,1 +1,7 @@
-//main js
+function sendFormReport() {
+    let clientReportName = document.getElementById('nome_cliente').value;
+    let clientReportEmail = document.getElementById('email_cliente').value;
+    let clientReportTel = document.getElementById('tel_cliente').value;
+    let clientReportMsg = document.getElementById('mensagem_cliente').value;
+    alert(clientReportName + '\n' + clientReportEmail + '\n' + clientReportTel + '\n' + clientReportMsg)
+}
