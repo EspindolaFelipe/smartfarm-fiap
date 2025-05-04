3f38a7445b96bbfdc4277134a949563184afa929
Cristian <camesjaames@gmail.com>
2025-05-04 03:44:44 -0300

    Alert adicionado e funcionalidades extras.

--- js/main.js
+++ js/main.js
@@ -5,3 +5,13 @@ function sendFormReport() {
     let clientReportMsg = document.getElementById('mensagem_cliente').value;
     alert(clientReportName + '\n' + clientReportEmail + '\n' + clientReportTel + '\n' + clientReportMsg)
 }
+
+function mostrarPopup() {
+    document.getElementById("fundo_blur").style.display = "block";
+    document.getElementById("div_popup").style.display = "block";
+  }
+  
+  function fecharPopup() {
+    document.getElementById("fundo_blur").style.display = "none";
+    document.getElementById("div_popup").style.display = "none";
+  }
