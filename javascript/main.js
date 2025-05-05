6fda1ad620b4c3d7f6e2982ebb924f74e9cf9e6b
Cristian <camesjaames@gmail.com>
2025-05-05 18:31:43 -0300

    Responsividade

---
+++ javascript/main.js
@@ -0,0 +1,17 @@
+function sendFormReport() {
+    let clientReportName = document.getElementById('nome_cliente').value;
+    let clientReportEmail = document.getElementById('email_cliente').value;
+    let clientReportTel = document.getElementById('tel_cliente').value;
+    let clientReportMsg = document.getElementById('mensagem_cliente').value;
+    alert(clientReportName + '\n' + clientReportEmail + '\n' + clientReportTel + '\n' + clientReportMsg)
+}
+
+function mostrarPopup() {
+    document.getElementById("fundo_blur").style.display = "block";
+    document.getElementById("div_popup").style.display = "block";
+  }
+  
+  function fecharPopup() {
+    document.getElementById("fundo_blur").style.display = "none";
+    document.getElementById("div_popup").style.display = "none";
+  }
